dbd65300ed3ec12c493244e335f4046152b9cdbe
Sarth Bishnoi <bishnoisarth@gmail.com>
2022-01-14 12:15:18 +1100

    task 1 "free code camp"

---
+++ javascript/1.js
@@ -0,0 +1,7 @@
+//Declare JavaScript Variables
+
+/*
+In computer science, data is anything that is meaningful to the computer. JavaScript provides eight different data types which are undefined, null, boolean, string, symbol, bigint, number, and object.
+*/
+
+var ourName;
